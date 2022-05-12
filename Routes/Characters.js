e0d354b51510f621fdee760df78d63ff98fd016f
Clément Notin <clem.notin@gmail.com>
2022-05-12 22:38:39 +0200

    make it better

--- Routes/Characters.js
+++ Routes/Characters.js
@@ -7,9 +7,9 @@ router.get("/characters", async (req, res) => {
     try{
         req.query.name?name=req.query.name: name = "";
         req.query.limit?limit=req.query.limit: limit = 100;
-        req.query.skip? skip=req.query.skip: skip = 0;
+        req.query.skip? skip=req.query.skip: skip = (req.query.page -1) * 100;
         const character = await axios.get(
-        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${apiKey}&name=${name}&limit=${limit}&skip=${(req.query.page - 1) * 100}`);
+        `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${apiKey}&name=${name}&skip=${skip}`);
         // &title=${title}&limit=${limit}&skip=${skip}`);
         console.log("sdfdsd",character.data);
         res.status(200).json(character.data);
