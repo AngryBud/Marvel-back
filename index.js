b69736ac3aef557558148536134b59744c5c6dfe
Clément Notin <clem.notin@gmail.com>
2022-05-13 10:42:59 +0200

    back ok

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const comicsRoute = require("./Routes/Comics");
+const mongoose = require("mongoose");
 const charactersRoute = require("./Routes/Characters");
 
 app.use(express());
 app.use(cors());
 app.use(comicsRoute);
 app.use(charactersRoute);
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 app.get("/", (req, res) => {res.status(200).json("Bienvenue sur l'Api Marvel")});
 
