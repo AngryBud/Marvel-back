276e068ac0d9bd92fec3bbe24806a6f0d24365a8
Clément Notin <clem.notin@gmail.com>
2022-05-14 23:38:01 +0200

    erreur index.js

--- models/Fav.js
+++ models/Fav.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 const Favori = mongoose.model("Favori", {
   name: String,
   description: String,
-//   fav_price: Number,
-//   fav_details: Array,
   image: String,
   owner: {
     type: mongoose.Schema.Types.ObjectId,
