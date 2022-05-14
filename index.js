9b3182e6dea179f4102d5e2f1be7c5862cf33d6f
Clément Notin <clem.notin@gmail.com>
2022-05-14 14:05:03 +0200

    add signup-login -> ajouter des verif supp

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
+const formidable=require("express-formidable");
 const app = express();
+const mongoose = require("mongoose");
 const cors = require("cors");
 const comicsRoute = require("./Routes/Comics");
 const charactersRoute = require("./Routes/Characters");
+const usersRoute = require("./Routes/Users");
 
+app.use(formidable());
 app.use(express());
 app.use(cors());
 app.use(comicsRoute);
 app.use(charactersRoute);
+app.use(usersRoute);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.get("/", (req, res) => {res.status(200).json("Bienvenue sur l'Api Marvel")});
 
