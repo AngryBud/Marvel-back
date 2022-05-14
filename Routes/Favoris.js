0dfddd00f508fd34272fd946fbe5d093844da6f6
Clément Notin <clem.notin@gmail.com>
2022-05-14 23:28:58 +0200

    erreur index.js

--- Routes/Favoris.js
+++ Routes/Favoris.js
@@ -5,7 +5,7 @@ const Fav = require("../models/Fav");
 const User = require("../models/User");
 const validateToken = require("../middlewares/validateToken");
 
-router.post("/addFavoris",validateToken, async(req,res) =>{
+router.post("/favoris/add",validateToken, async(req,res) =>{
     
     try{
         const {fav_name,fav_description, fav_image} = req.fields;
@@ -30,4 +30,28 @@ router.post("/addFavoris",validateToken, async(req,res) =>{
     }
 });
 
+// router.post("/favoris/all", async(req, res)=>{
+//     try{
+
+//         const authoUser = await Fav.findOne({token:});
+//         if (!authoUser)
+//             return res.status(401).json("No account with this mail Please Signup !");
+//         if (decrypt === authoUser.hash){
+//             const ret = 
+//                 {_id: authoUser._id,
+//                     token: authoUser.token,
+//                     account:{
+//                         username: authoUser.account.username,
+//                     state: "Connect with success"
+//                 }}
+//                 return res.status(200).json(ret);
+//             }
+//             else{
+//                 return res.status(401).json("Invalid password");
+//             }
+        
+//     }catch(error){
+//         return res.status(400).json({error: {message: error.message}});
+//     }
+// }
 module.exports = router;
