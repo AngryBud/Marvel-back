cb0d34d59031c44a5e8542cb6c385f96da9666cc
Clément Notin <clem.notin@gmail.com>
2022-05-15 18:17:07 +0200

    bon demande a aller plus loin toujours

--- models/Fav.js
+++ models/Fav.js
@@ -4,11 +4,11 @@ const Favori = mongoose.model("Favori", {
   name: String,
   description: String,
   image: String,
-//   owner: String
-  owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
+  owner: String
+//   owner: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: "User",
+//   },
 });
 
 module.exports = Favori;
